06e8b929a5134a4bed787ac7a5be68e0620883fa
Taha <77605797+barlaktaha@users.noreply.github.com>
2021-02-24 00:47:59 +0300

    Summary

--- MyFinalProject/DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ MyFinalProject/DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,11 +17,11 @@ namespace DataAccess.Concrete.InMemory
             // Oracle,Sql Server, Postgres, MongoDb
             _products = new List<Product> 
             {
-                new Product {ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitInStock=15},
-                new Product {ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitInStock=3},
-                new Product {ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitInStock=2},
-                new Product {ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitInStock=65},
-                new Product {ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitInStock=1},
+                new Product {ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product {ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product {ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product {ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product {ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1},
             };
         }
         public void Add(Product product)
@@ -69,13 +70,18 @@ namespace DataAccess.Concrete.InMemory
             return _products.Where(p => p.CategoryId == categoryId).ToList();
         }
 
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            throw new NotImplementedException();
+        }
+
         public void Update(Product product)
         {
             Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
             productToUpdate.ProductName = product.ProductName;
             productToUpdate.ProductId = product.ProductId;
             productToUpdate.CategoryId = product.CategoryId;
-            productToUpdate.UnitInStock = product.UnitInStock;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
             productToUpdate.UnitPrice = product.UnitPrice;
 
         }
